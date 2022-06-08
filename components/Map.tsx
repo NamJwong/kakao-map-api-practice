dd85338af2428cc37d9493b9c9430ccd6cf5b7dd
jynam <njy1007@gmail.com>
2022-06-08 19:16:10 +0900

    feat: Script 컴포넌트 활용해서 API 업로드 하기

--- components/Map.tsx
+++ components/Map.tsx
@@ -13,29 +13,18 @@ interface MapProps {
 
 export default function Map({ latitude, longitude }: MapProps) {
   useEffect(() => {
-    const mapScript = document.createElement('script');
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
-    document.head.appendChild(mapScript);
-
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(latitude, longitude),
-        };
-        const map = new window.kakao.maps.Map(container, options);
-        const markerPosition = new window.kakao.maps.LatLng(
-          latitude,
-          longitude
-        );
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-        });
-        marker.setMap(map);
+    window.kakao.maps.load(() => {
+      const container = document.getElementById('map');
+      const options = {
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+      };
+      const map = new window.kakao.maps.Map(container, options);
+      const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition,
       });
-    };
-    mapScript.addEventListener('load', onLoadKakaoMap);
+      marker.setMap(map);
+    });
   }, [latitude, longitude]);
 
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`}
+        strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
